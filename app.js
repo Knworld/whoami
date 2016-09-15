97f81052d3ca2f27b6fb61a03f1fa7ca7580148c
Khuyen Nguyen <khuyenvnguyen@gmail.com>
2016-09-15 05:57:41 +0000

    complete

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+
+
+app.get('/', function(req, res){
+   res.redirect('/api/whoami') ;
+});
+
+app.get('/api/whoami', function(req, res){
+    var resObj = {};
+    resObj.ipaddress = req.headers['x-forwarded-for'];
+    resObj.language = req.headers['accept-language'].split(',')[0];
+    resObj.software =  req.headers['user-agent'].match(/(\()[\w+\s.;]+/)[0].replace('(', '');
+    
+    res.json(resObj);
+}).listen(process.env.port || 8080);
+    
